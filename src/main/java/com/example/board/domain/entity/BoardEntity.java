94517af57d8bcc48e48ade3443689d37eeb06690
kimhansol <kimhansol@neighbor21.co.kr>
2022-12-01 15:01:40 +0900

    변경

--- src/main/java/com/example/board/domain/entity/BoardEntity.java
+++ src/main/java/com/example/board/domain/entity/BoardEntity.java
@@ -1,19 +1,20 @@
 package com.example.board.domain.entity;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
-@Table(name = "BoardEntity")
+@Table(name = "board")
 public class BoardEntity extends TimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 10, nullable = false)
@@ -24,6 +25,47 @@ public class BoardEntity extends TimeEntity {
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
+
+    @Builder
+    public BoardEntity(Long id, String title, String content, String writer) {
+        this.id = id;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
 }
+
+/*import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class BoardEntity extends TimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+    @Column(length = 10, nullable = false)
+    private String writer;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+
+}*/
 
 
